3f49a7d8b1238b517d5ded8db165ed80dc78da29
wisniewskikr <wisniewskikr@gmail.com>
2024-01-24 10:55:24 +0100

    Update Readme file

--- architectures/gui/html/ts-angular-gui-html-helloworld-single/src/app/app.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-single/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ts-angular-gui-html-helloworld-single';
+  message = 'Hello World!';
 }
