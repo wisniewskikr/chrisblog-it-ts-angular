f7446faeb41b87c1631bbf0454ecdbc2b46d6cfb
wisniewskikr <wisniewskikr@gmail.com>
2024-01-26 20:56:52 +0100

    Implement Init and HelloWorld routing.

--- architectures/gui/html/ts-angular-gui-html-helloworld-multiple/src/app/app.routes.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-multiple/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { InitComponent } from './init/init.component';
 
 export const routes: Routes = [
+    { path: '', component: InitComponent },
     { path: 'init', component: InitComponent },
     { path: 'helloworld', component: HelloWorldComponent },
 ];
--- architectures/gui/html/ts-angular-gui-html-helloworld-multiple/src/app/init/init.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-multiple/src/app/init/init.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-init',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './init.component.html',
   styleUrl: './init.component.css'
 })
