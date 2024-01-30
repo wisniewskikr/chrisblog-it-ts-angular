51f348e8505c72e22343c178090909e22d7cdbcc
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-01-30 08:19:44 +0100

    Init project

---
+++ technologies/ts-angular-reactive-forms/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  name = '';
+  message = '';
+
+  sendName() {
+    this.message = "Hello World " + this.name;
+    this.name = '';
+  }
+
+}
