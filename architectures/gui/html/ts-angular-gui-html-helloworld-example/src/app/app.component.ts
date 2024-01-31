fbfacfe6ea469e8fce819de8f76b316bc2855f44
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-01-31 18:51:56 +0100

    Init project

---
+++ architectures/gui/html/ts-angular-gui-html-helloworld-example/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+  message = '';
+
+  sendName(name: string) {
+    this.message = "Hello World " + name;
+  }
+
+}
