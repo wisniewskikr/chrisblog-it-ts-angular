7698dd575a34446fba41f42c86f073a39310130b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-01 08:40:21 +0100

    Init create component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ListComponent } from './list/list.component';
+import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
     { path: '', component: ListComponent },
+    { path: 'create', component: CreateComponent },
     { path: '**', component: NotFoundComponent }
 ];
--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/list/list.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
