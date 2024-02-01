496982e3f8969ce8ccc49fe857fcad8d7f2529c9
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-01 08:59:28 +0100

    Update Crud service

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/services/crud.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/services/crud.ts
@@ -5,36 +5,27 @@ export class Crud {
     messages: Message[] = [];
     messageId = 1;
 
+    readAll() {
+        return this.messages;
+    }
+
+    readById(id: number) {
+        return this.messages.find((c) => c.id == id);
+    }
+
+    create(text: string) {
+        const message = JSON.parse(`{"id": ${this.messageId}, "text": "${text}"}`)
+        this.messages.push(message);
+        this.messageId++;
+    }
+
+    update(message: Message) {
+        this.messages = this.messages.filter((c) => c.id != message.id);
+        this.messages.push(message); 
+    }
+
+    delete(id: number) {
+        this.messages = this.messages.filter((c) => c.id != id);
+    }
+
 }
-
-
-
-// type Message = {
-//     id: number;
-//     text: string;
-// }
-// let messages: Message[] = [];
-// let messageId = 1;
-
-// export const readAll = () => {
-//     return messages;
-// }
-
-// export const readById = (id: number) => {
-//     return messages.find((c) => c.id == id);
-// }
-
-// export const create = (text: string) => {
-//     const message = JSON.parse(`{"id": ${messageId}, "text": "${text}"}`)
-//     messages.push(message);
-//     messageId++;
-// }
-
-// export const update = (message: Message) => {
-//     messages = messages.filter((c) => c.id != message.id);
-//     messages.push(message);
-// }
-
-// export const del = (id: number) => {
-//     messages = messages.filter((c) => c.id != id);
-// }
