ae9264cff3516625cdebb861f3b7a261f169a71a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-01 17:54:17 +0100

    Fix create action

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/list/list.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/list/list.component.ts
@@ -3,22 +3,24 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Message } from '../../models/Message';
 import { CrudService } from '../../services/CrudService';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent implements OnInit {  
 
   messages: Message[] = [];
+  selectedMessageId: number | null = null;
 
   constructor(private crudService: CrudService) { }
 
   ngOnInit() {
     this.messages = this.crudService.readAll();
-  }  
+  } 
 
 }
