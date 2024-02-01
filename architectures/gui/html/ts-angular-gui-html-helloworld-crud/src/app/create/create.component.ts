978dab3062bbfd903646efe918c07ad347eab891
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-01 16:36:19 +0100

    Implement Create component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/create/create.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/create/create.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CrudService } from '../../services/CrudService';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  constructor(private crudService: CrudService) { }
+
+  create(text: string) {
+    this.crudService.create(text);
+  }
 
 }
