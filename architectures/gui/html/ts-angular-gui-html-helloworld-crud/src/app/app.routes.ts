5c415ce198f43d4318ac0fe17459f407659bb2e4
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-01 08:22:03 +0100

    Init list

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
-import { InitComponent } from './init/init.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ListComponent } from './list/list.component';
 
 export const routes: Routes = [
-    { path: '', component: InitComponent },
-    { path: 'init', component: InitComponent },
-    { path: 'helloworld', component: HelloWorldComponent },
+    { path: '', component: ListComponent },
     { path: '**', component: NotFoundComponent }
 ];
