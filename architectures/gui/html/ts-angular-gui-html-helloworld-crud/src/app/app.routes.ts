d6a5d5b129eda394fa4d49aa160f31b49c74d498
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-02 08:30:05 +0100

    Init View component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
+import { ViewComponent } from './view/view.component';
 
 export const routes: Routes = [
     { path: '', component: ListComponent },
     { path: 'create', component: CreateComponent },
+    { path: 'view', component: ViewComponent },
     { path: '**', component: NotFoundComponent }
 ];
