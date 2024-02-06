e2e666746a199d89b99119daf5aa544fdff38338
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-06 08:08:57 +0100

    Display in Update component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/update/update.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/update/update.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Message } from '../../models/Message';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CrudService } from '../../services/CrudService';
 
 @Component({
   selector: 'app-update',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './update.component.html',
   styleUrl: './update.component.css'
 })
 export class UpdateComponent {
+
+  message: Message | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private crudService: CrudService) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.message = crudService.readById(params['id']);
+    }); 
+
+  }
 
 }
