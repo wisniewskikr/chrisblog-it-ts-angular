182d9d98d6c880affcb35905b056909d90458511
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-06 08:21:39 +0100

    Init Delete component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { ListComponent } from './list/list.component';
 import { CreateComponent } from './create/create.component';
 import { ViewComponent } from './view/view.component';
 import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
 
 export const routes: Routes = [
     { path: '', component: ListComponent },
     { path: 'create', component: CreateComponent },
     { path: 'view/:id', component: ViewComponent },
     { path: 'update/:id', component: UpdateComponent },
+    { path: 'delete/:id', component: DeleteComponent },
     { path: '**', component: NotFoundComponent }
 ];
