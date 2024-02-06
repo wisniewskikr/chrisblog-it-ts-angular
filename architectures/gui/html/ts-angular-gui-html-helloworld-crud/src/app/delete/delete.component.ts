97ab7bde52a88558e8e8f3a0c1838c9315e55c40
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-06 08:29:22 +0100

    Finish Delete component

--- architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/delete/delete.component.ts
+++ architectures/gui/html/ts-angular-gui-html-helloworld-crud/src/app/delete/delete.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { Message } from '../../models/Message';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { CrudService } from '../../services/CrudService';
 
 @Component({
   selector: 'app-delete',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './delete.component.html',
   styleUrl: './delete.component.css'
 })
 export class DeleteComponent {
+
+  message: Message | null = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private crudService: CrudService, private router: Router) {
+
+    this.activatedRoute.params.subscribe(params => {
+      this.message = crudService.readById(params['id']);
+    }); 
+
+  }
+
+  delete(id: string) {
+    this.crudService.delete(Number(id));
+    this.router.navigate(['']);
+  }
 
 }
