3288ad2dbedf62fb87e9d1db379af226282cb865
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-20 20:59:38 +0100

    Add CrossOrigin

--- technologies/angular-springboot-separate/java-springboot-api-rest-angular-separate/src/main/java/com/example/controllers/HelloWorldController.java
+++ technologies/angular-springboot-separate/java-springboot-api-rest-angular-separate/src/main/java/com/example/controllers/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.example.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @CrossOrigin("http://localhost:4200/")
     @GetMapping("/")
     public String displayMessage() {
         return "Hello World!";
