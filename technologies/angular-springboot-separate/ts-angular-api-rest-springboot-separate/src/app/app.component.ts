894450567f1b3752865d96cc416fe928e26713d8
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-20 20:57:44 +0100

    Implement HttpClient

--- technologies/angular-springboot-separate/ts-angular-api-rest-springboot-separate/src/app/app.component.ts
+++ technologies/angular-springboot-separate/ts-angular-api-rest-springboot-separate/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +9,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  message = 'Hello World!';
+export class AppComponent implements OnInit{
+
+  message: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    let response = this.http.get("http://localhost:8080/");
+    response.subscribe((data)=>this.message = data);
+  }
+  
 }
