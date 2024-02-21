a408435e962f4e6b7ae88e371a3afaf39ea11d41
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-02-21 21:04:33 +0100

    Implement error handling

--- technologies/angular-springboot-separate/ts-angular-api-rest-springboot-separate/src/app/services/api.service.ts
+++ technologies/angular-springboot-separate/ts-angular-api-rest-springboot-separate/src/app/services/api.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
+  URL = 'http://localhost:8080/';
+
   constructor(private http: HttpClient) {}
 
   getMessage() {
-    return this.http.get("http://localhost:8080/", {responseType: 'text'});
+    return this.http.get(this.URL, {responseType: 'text'})
+          .pipe(
+            catchError((error) => {
+              return throwError(() => error);
+            })
+          );
   }
+
 }
