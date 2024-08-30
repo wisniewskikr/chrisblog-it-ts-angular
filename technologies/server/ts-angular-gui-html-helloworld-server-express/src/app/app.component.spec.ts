6ec7b464170491b85def13f9ffae620ad16f5864
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-30 17:42:14 +0200

    Init project

--- technologies/server/ts-angular-gui-html-helloworld-server-express/src/app/app.component.spec.ts
+++ technologies/server/ts-angular-gui-html-helloworld-server-express/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ts-angular-gui-html-helloworld-server' title`, () => {
+  it(`should have the 'ts-angular-gui-html-helloworld-server-express' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ts-angular-gui-html-helloworld-server');
+    expect(app.title).toEqual('ts-angular-gui-html-helloworld-server-express');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ts-angular-gui-html-helloworld-server');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ts-angular-gui-html-helloworld-server-express');
   });
 });
