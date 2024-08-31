54264ac394d6ecd894f1294d2d333c8ab4a9549d
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-31 18:35:34 +0200

    Simplify entrypoint

--- technologies/env/ts-angular-gui-html-helloworld-env-docker/entrypoint.sh
+++ technologies/env/ts-angular-gui-html-helloworld-env-docker/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 # Replace placeholder in config.json with environment variables
-sed -i "s|\"message\": \".*\"|\"message\": \"$MESSAGE\"|" /usr/share/nginx/html/assets/config.template.json
-envsubst < /usr/share/nginx/html/assets/config.template.json > /usr/share/nginx/html/assets/config.json
+sed -i "s|\"message\": \".*\"|\"message\": \"$MESSAGE\"|" /usr/share/nginx/html/assets/config.json
 
 # Start nginx
 exec "$@"
