f49d5b9d631f3624f392dbb1e54e68392c4cc02b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-09-02 08:39:52 +0200

    Remove project

--- technologies/env/ts-angular-gui-html-helloworld-env-docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace placeholder in config.json with environment variables
-sed -i "s|\"message\": \".*\"|\"message\": \"$MESSAGE\"|" /usr/share/nginx/html/assets/config.json
-
-# Start nginx
-exec "$@"
